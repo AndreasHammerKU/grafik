fce60c1ba581b09fc6e1254299940762c981925a
AndreasHammerKU <andreashammerskole@gmail.com>
2024-12-10 09:51:11 +0100

    assignment 3 implementation

--- GraphicsProject/DIKUgraphics/src/camera.cpp
+++ GraphicsProject/DIKUgraphics/src/camera.cpp
@@ -217,8 +217,7 @@ glm::mat4x4 Camera::InvWindowViewport() const
  */
 glm::mat4x4 Camera::CurrentTransformationMatrix()
 {
-    std::cout << "Camera::CurrentTransformationMatrix(): Not implemented yet!" << std::endl;
-    
+    this->currenttransformationmatrix = this->WindowViewport() * this->ViewProjection() * this->ViewOrientation();
     return this->currenttransformationmatrix;
 }
 
@@ -234,8 +233,7 @@ glm::mat4x4 Camera::InvCurrentTransformationMatrix()
     TraceMessage("InvViewProjection() = " << std::endl << this->invviewprojectionmatrix << std::endl;);
     TraceMessage("InvWindowViewport() = " << std::endl << this->invwindowviewportmatrix << std::endl;);
 
-    std::cout << "Camera::InvCurrentTransformationMatrix(): Not implemented yet!" << std::endl;
-    
+    this->invcurrenttransformationmatrix = this->InvViewOrientation() * this->InvViewProjection() * this->InvWindowViewport();
     return this->invcurrenttransformationmatrix;
 }
 
@@ -466,9 +464,19 @@ void Camera::ViewportHeight(int new_viewport_height)
  */
 void Camera::ComputeViewOrientation(glm::vec3& vrp, glm::vec3& vpn, glm::vec3& vup)
 {
-    Trace("Camera", "ComputeViewOrientation(vec3&, vec3&, vec3&)");
+ glm::vec3 n = glm::normalize(vpn);
+ glm::vec3 u = glm::normalize(glm::cross(vup, n));
+ glm::vec3 v = glm::cross(n, u);
 
-    std::cout << " Camera::ComputeViewOrientation(vec3&, vec3&, vec3&): Not implemented yet!" << std::endl;
+ glm::mat4x4 orientation = glm::mat4x4(1.0f);
+ orientation[0] = glm::vec4(u, 0.0f);
+  orientation[1] = glm::vec4(v, 0.0f);
+  orientation[2] = glm::vec4(n, 0.0f);
+  orientation[3] = glm::vec4(0.0f,0.0f,0.0f, 1.0f);
+
+  glm::mat4x4 translation = glm::translate(glm::mat4x4(1.0f), -vrp);
+
+  this->vieworientationmatrix = orientation * translation;
 }
 
 /*
@@ -485,8 +493,32 @@ void Camera::ComputeViewProjection(glm::vec3& prp,
                    float front_clipping_plane, float back_clipping_plane)
 {
     Trace("Camera", "ComputeViewProjection(vec3&, vec2&, vec2&, float, float)");
+    
+    glm::mat4x4 T_prp = glm::translate(glm::mat4x4(1.0f), -prp);
 
-    std::cout << "Camera::ComputeViewProjection(vec3&, vec2&, vec2&, float, float): Not Implemented yet!" << std::endl;
+    float centerX = (lower_left_window.x + upper_right_window.x) / 2.0f;
+    float centerY = (lower_left_window.y + upper_right_window.y) / 2.0f;
+    glm::mat4x4 Sh_per = glm::mat4x4(1.0f);
+    glm::vec3 cw = glm::vec3(centerX, centerY, 0.0f);
+    glm::vec3 dop = prp - cw;
+    Sh_per[2][0] = - dop.x / dop.z;
+    Sh_per[2][1] = - dop.y / dop.z;
+
+    float width = upper_right_window.x - lower_left_window.x;
+    float height = upper_right_window.y - lower_left_window.y;
+
+    glm::mat4x4 S_per = glm::mat4x4(1.0f);
+    S_per[0][0] = -2*prp.z / (width * back_clipping_plane);
+    S_per[1][1] = -2*prp.z / (height * back_clipping_plane);
+    S_per[2][2] = -1 / (back_clipping_plane);
+
+    glm::mat4x4 M_perpar = glm::mat4x4(1.0f);
+    M_perpar[2][2] = 1 / ( 1 + front_clipping_plane);
+    M_perpar[2][3] = -1.0f;
+    M_perpar[3][2] = -front_clipping_plane / (1 + front_clipping_plane);
+    M_perpar[3][3] = 0.0f;
+
+    this->viewprojectionmatrix = M_perpar * S_per * Sh_per * T_prp;
 }
 
 /*
